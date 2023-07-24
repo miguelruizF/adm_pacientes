e6fad51d8c729388596b923e01314a17f16d807f
Miguel Ruiz <later481@gmail.com>
2023-07-23 20:39:33 -0600

    funciones UI

--- app.js
+++ app.js
@@ -18,7 +18,46 @@ function eventListener(){
     inputFecha.addEventListener("input", datosCitas);
     inputHora.addEventListener("input", datosCitas);
     inputSintomas.addEventListener("input", datosCitas);
+    formulario.addEventListener("submit", nuevaCita);
 }
+
+//Clases
+class Citas{
+    constructor(){
+        this.citas = [];
+    }
+
+    agregarCita(cita){
+        this.citas = [...this.citas, cita];
+        console.log(this.citas)
+    }
+}
+
+class UI{
+    imprimirAlerta(mensaje, tipo){
+        const divMensaje = d.createElement("div");
+        divMensaje.classList.add("w-9/12","text-center", "border-t-4", "rounded-b", "py-3", "px-20","shadow-md", "mb-4", "text-lg", "font-bold");
+
+        if(tipo === "error"){
+            divMensaje.classList.add("bg-red-100", "border-red-700" )
+        }else{
+            divMensaje.classList.add("bg-teal-100", "border-teal-500")
+        }
+
+        divMensaje.textContent = mensaje;
+
+        //Agregar al DOM
+        d.querySelector("#contenido").insertBefore(divMensaje, d.querySelector("#section-formulario"));
+
+        //Quitar alerta
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
+
+const ui = new UI();
+const admCitas = new Citas();
 
 //Objeto - informacion del formulario
 const infoObj = {
@@ -33,5 +72,38 @@ const infoObj = {
 //Funciones 
 function datosCitas(e){
     infoObj[e.target.name] = e.target.value;
-    console.log(infoObj);
+    // console.log(infoObj);
+}
+
+function nuevaCita(e){
+    e.preventDefault();
+    const {mascota, propietario,telefono,fecha,hora,sintomas} = infoObj;
+
+    //validar
+    if(mascota === "" || propietario === "" || telefono === "" || fecha ==="" || hora === "" || sintomas === ""){
+        ui.imprimirAlerta("Todos los campos son obligatorios", "error");
+        // console.log("Todos los datos son obligatorios");
+        return;
+    }
+
+    //Generar Id
+    infoObj.id = Date.now();
+
+    //Crear nueva cita
+    admCitas.agregarCita({...infoObj});
+
+    //Reiniciar objeto
+    reiniciarObjeto();
+    // console.log(admCitas)
+    ui.imprimirAlerta("Cita agregada correctamente");
+    formulario.reset();
+}
+
+function reiniciarObjeto(){
+    infoObj.mascota = "",
+    infoObj.propietario = "",
+    infoObj.telefono = "",
+    infoObj.fecha = "",
+    infoObj.hora = "",
+    infoObj.sintomas = ""
 }
